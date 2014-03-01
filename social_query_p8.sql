6ab27419aca93b7a8949f3bdda64988cca6dd506
Tyson Hartwig <tysonhartwig@users.noreply.github.com>
2014-03-01 16:47:08 -0500

    Delete social_query_p8.sql

--- social_query_p8.sql
+++
@@ -1,9 +0,0 @@
-/*  Problem 8
-
-Find the difference between the number of students in the school and the number of different first names. */
-
-select count(*)
-from   Highschooler H1, Highschooler H2
-where  H1.name = H2.name
-and    H1.ID   > H2.ID;
-
