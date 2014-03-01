6f78ceaccedd5b2805759ef5f44cb287c75a4a19
tysonhartwig <tyson.hartwig@gmail.com>
2014-03-01 17:05:05 -0500

    fixing up directory structure

--- basic-sql/rating_query_p1.sql
+++
@@ -1,7 +0,0 @@
-/* Problem 1
-
-Find the titles of all movies directed by Steven Spielberg.
-*/
-select Movie.title 
-from Movie 
-where Movie.director="Steven Spielberg";
--- basic-sql/rating_query_p5.sql
+++
@@ -1,3 +0,0 @@
-select name, title, stars, ratingDate 
-from Movie natural join Reviewer natural join Rating 
-order by name, title, stars desc;
