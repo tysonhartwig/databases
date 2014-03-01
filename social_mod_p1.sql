4fbdea81071394129032d16cf9bfc266ea22e772
Tyson Hartwig <tysonhartwig@users.noreply.github.com>
2014-03-01 16:40:38 -0500

    Delete social_mod_p1.sql

--- social_mod_p1.sql
+++
@@ -1,8 +0,0 @@
-/* Problem 1
-
-It's time for the seniors to graduate. Remove all 12th graders from Highschooler.
-This operation will orphan the students Likes and friends*/
-
-delete from Highschooler
-where  grade = 12;
-
