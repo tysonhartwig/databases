51a2ff4a1c8058fe418457260f7ee9f58f6be298
tysonhartwig <tyson.hartwig@gmail.com>
2014-03-02 20:37:05 -0500

    trying to write once per items-*.xml file

--- project/etree_parser.py
+++ project/etree_parser.py
@@ -8,8 +8,9 @@ from __future__ import print_function
 import datetime
 import locale
 import sys
+import time
 try:                                 # cElementTree is faster but 
-    import xml.etree.cElementTree    # not standard.
+    import xml.etree.cElementTree    # not always available.
 except ImportError:
     import xml.etree.ElementTree
 
@@ -30,50 +31,52 @@ def transform_dttm(dttm_str):
 
 class Processor: 
     def __init__(self):
-        # self.categoryTable = list()
         self.categoryFile = open('category.txt', 'a+')
         self.auctionsFile = open('auctions.txt', 'a+')
+        self.delimiter    = '<>'
 
     def iterparent(self, tree):
-        for parent in tree.getiterator():
+        for parent in tree.iter():
             for child in parent:
-                yield parent, child
+                if parent.tag != 'Items': yield parent, child
 
     def processFile(self, filename):
-        domItems   = xml.etree.ElementTree.parse(filename) 
-        root = domItems.getroot()
+        domItems    = xml.etree.ElementTree.parse(filename) 
+        root        = domItems.getroot()
+        categoryRow = list() 
+        auctionsRow = list()
         for parent, child in self.iterparent(root):
-            print(parent.tag, child.tag)
             if child.tag == 'Name':
-                itemID = parent.get('ItemID')
-                row = itemID + '<>' + child.text
+                itemID     = parent.get('ItemID')
+                itemNameID = itemID + self.delimiter + child.text
             elif child.tag == 'Category':
-                print(itemID, '<>', child.text, 
-                     file = self.categoryFile)
-            elif child.tag == 'First_Bid' or child.tag == 'Description':  
-                print('<>', child.text, 
-                     file = self.auctionsFile, end = ' ')
-            elif child.tag == 'Started' or child.tag == 'Ends':
-                print('<>', transform_dttm(child.text), 
-                     file = self.auctionsFile, end = ' ')
+                categoryRow.append(itemID + self.delimiter + child.text)
+            elif child.tag == 'First_Bid':
+                itemMinBid = self.delimiter + child.text
+            elif child.tag == 'Starts':
+                startDate = self.delimiter + transform_dttm(child.text) 
+            elif child.tag == 'Ends':
+                endDate = self.delimiter + transform_dttm(child.text) 
             elif child.tag == 'Description':
-                row = row + child.text
-                print(row, '\n', file = self.auctionsFile)
-        """
-            else: print('Unknown Tag?')
+                auctionsRow.append(itemNameID + itemMinBid + startDate 
+                                              + endDate    + self.delimiter 
+                                              + child.text)
+                print(child.text)
+            elif parent.tag == 'Bids':
+                print(child.text)
+        self.categoryFile.write("\n".join(categoryRow))
+        self.auctionsFile.write("\n".join(auctionsRow))
 
-        for item in root:
-            for category in item.findall('Category'):
-               print(item.get('ItemID'),'<>', category.text, 
-                     file=self.categoryFile)
-        """
 def main():
+    startTime = time.time()
     if len(sys.argv) <= 1:
         print("Usage: python3", sys.argv[0], "[file] [file] ...")
     ebay = Processor()     
     locale.setlocale(locale.LC_ALL, 'en_US.UTF8')
     for filename in sys.argv[1:]:
         ebay.processFile(filename)
+    endTime = time.time()
+    print("Elapsed time was %g seconds" % (endTime - startTime))
 
 if __name__ == "__main__":
     main()
